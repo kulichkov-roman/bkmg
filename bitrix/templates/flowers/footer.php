155a1c0eb101f61b41d8512c0c931b71f9df2c4e
Roman Kulichkov <roman@kulichkov.pro>
2016-04-19 17:01:18 +0600

    Замена года в подвле сайта

--- bitrix/templates/flowers/footer.php
+++ bitrix/templates/flowers/footer.php
@@ -2,7 +2,7 @@
 	<footer id="footer">
     <div class="vk"><a href="https://vk.com/cveti_mgn" target="_blank">Мы ВКонтакте</a></div>
     <div class="coop">
-        2014 &copy; Цветочный бутик<br />
+        2016 &copy; Цветочный бутик<br />
         Дизайн и разработка  <a target="_blank" title="Разработка сайтов" href="http://www.madcolor.ru">&laquo;MadColor&raquo;</a>
     </div>
 	<div class="yal_link_footer">
